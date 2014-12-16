923ea33c6a35a29a6bfceedfd14111dab3aa0b55
Park JunHyun <junhyun.park@jam2in.com>
2014-12-16 21:18:56 +0900

    acp-java: change arcus-java-client path and jarfile name.

--- acp-java/compile.bash
+++ acp-java/compile.bash
@@ -2,11 +2,11 @@
 
 DIR=`readlink -f $0`
 DIR=`dirname $DIR`
-if test -d "$DIR/../arcus-java-client" ; then
-  JARFILE=$DIR/../arcus-java-client/target/arcus-client-1.7.0.jar
+if test -d "$DIR/../../arcus-java-client" ; then
+  JARFILE=$DIR/../../arcus-java-client/target/arcus-java-client-1.7.0.jar
 else
-  if test -d "$DIR/../java-memcached-client" ; then
-    JARFILE=$DIR/../java-memcached-client/target/arcus-client-1.6.3.0.jar
+  if test -d "$DIR/../../java-memcached-client" ; then
+    JARFILE=$DIR/../../java-memcached-client/target/arcus-client-1.6.3.0.jar
   else
     echo "Cannot find arcus jar file."
     exit
--- acp-java/run.bash
+++ acp-java/run.bash
@@ -1,8 +1,8 @@
 DIR=`readlink -f $0`
 DIR=`dirname $DIR`
-if test -d "$DIR/../arcus-java-client" ; then
-  JAR_DIR=$DIR/../arcus-java-client/target
-  CP=$JAR_DIR/arcus-client-1.7.0.jar:$JAR_DIR/zookeeper-3.4.5.jar:$JAR_DIR/log4j-1.2.16.jar:$JAR_DIR/slf4j-api-1.6.1.jar:$JAR_DIR/slf4j-log4j12-1.6.1.jar
+if test -d "$DIR/../../arcus-java-client" ; then
+  JAR_DIR=$DIR/../../arcus-java-client/target
+  CP=$JAR_DIR/arcus-java-client-1.7.0.jar:$JAR_DIR/zookeeper-3.4.5.jar:$JAR_DIR/log4j-1.2.16.jar:$JAR_DIR/slf4j-api-1.6.1.jar:$JAR_DIR/slf4j-log4j12-1.6.1.jar
 else
   if test -d "$DIR/../java-memcached-client" ; then
     JAR_DIR=$DIR/../java-memcached-client/target
