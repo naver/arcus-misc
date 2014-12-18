3f8aa72b2702acb277eaafd55443b008c0a10e94
Park JunHyun <junhyun.park@jam2in.com>
2014-12-18 17:23:24 +0900

    compare: change arcus-java-client jar filename.

--- compare/compile_compare.bash
+++ compare/compile_compare.bash
@@ -2,7 +2,7 @@ DIR=`readlink -f $0`
 DIR=`dirname $DIR`
 if test -d "$DIR/../../arcus-java-client" ; then
   JAR_DIR=$DIR/../../arcus-java-client/target
-  CP=$JAR_DIR/arcus-client-1.7.0.jar
+  CP=$JAR_DIR/arcus-java-client-1.7.0.jar
 else
   if test -d "$DIR/../../java-memcached-client" ; then
     JAR_DIR=$DIR/../../java-memcached-client/target
