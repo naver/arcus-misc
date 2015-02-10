5143272e9c56781995f51a7610c7464f4d55c4cd
Park JunHyun <junhyun.park@jam2in.com>
2015-02-10 19:20:36 +0900

    Change repl cluster's root znode name: arcus_1_7 => arcus_repl.

--- acp-java/setup-test-zk.bash
+++ acp-java/setup-test-zk.bash
@@ -4,27 +4,27 @@ DIR=`dirname $DIR`
 ZK_CLI="$DIR/../../arcus/zookeeper/bin/zkCli.sh"
 ZK_ADDR="-server localhost:2181"
 
-$ZK_CLI $ZK_ADDR create /arcus_1_7 0
+$ZK_CLI $ZK_ADDR create /arcus_repl 0
 
-$ZK_CLI $ZK_ADDR create /arcus_1_7/client_list 0
-$ZK_CLI $ZK_ADDR create /arcus_1_7/client_list/test 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/client_list 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/client_list/test 0
 
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_server_log 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_log 0
 
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_list 0
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_list/test 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_list 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_list/test 0
 # ehpemeral znode = <group>^M^<ip:port-hostname> 0 // created by cache node
 # ehpemeral znode = <group>^S^<ip:port-hostname> 0 // created by cache node
 
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_server_group 0
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_server_group/test 0
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_server_group/test/g0 0
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_server_group/test/g0/lock 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_group 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_group/test 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_group/test/g0 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_group/test/g0/lock 0
 # ehpemeral/sequence znode = <nodeip:port>^<listenip:port>^<sequence> 0
 # ehpemeral/sequence znode = <nodeip:port>^<listenip:port>^<sequence> 0
 
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_server_mapping 0
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_server_mapping/127.0.0.1:11215 0
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_server_mapping/127.0.0.1:11215/test^g0^127.0.0.1:20125^ 0
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_server_mapping/127.0.0.1:11216 0
-$ZK_CLI $ZK_ADDR create /arcus_1_7/cache_server_mapping/127.0.0.1:11216/test^g0^127.0.0.1:20126^ 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/127.0.0.1:11215 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/127.0.0.1:11215/test^g0^127.0.0.1:20125^ 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/127.0.0.1:11216 0
+$ZK_CLI $ZK_ADDR create /arcus_repl/cache_server_mapping/127.0.0.1:11216/test^g0^127.0.0.1:20126^ 0
