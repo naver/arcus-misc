7a4492046fee5226c40b5d7bdefb1382162eba27
Wookhwan, Choi <whchoi83@jam2in.com>
2015-05-28 16:51:25 +0900

    modify run.sh

--- act-java/AsyncBopPipedInsertBulkTest/run.sh
+++ act-java/AsyncBopPipedInsertBulkTest/run.sh
@@ -3,6 +3,8 @@ JAVA_CLIENT_LIB_DIR=$HOME/src/arcus-java-client/target
 
 LIB_CP=$JAVA_CLIENT:$JAVA_CLIENT_LIB_DIR/zookeeper-3.4.5.jar:$JAVA_CLIENT_LIB_DIR/log4j-1.2.16.jar:$JAVA_CLIENT_LIB_DIR/slf4j-api-1.6.1.jar:$JAVA_CLIENT_LIB_DIR/slf4j-log4j12-1.6.1.jar
 
-rm -f bin/*.class
+rm -rf ./bin
+mkdir bin
+
 javac -sourcepath src -d bin -Xlint:deprecation -cp $JAVA_CLIENT:. src/AsyncBopPipedInsertBulkTest.java
 java -ea -Xmx2g -Xms2g "-Dnet.spy.log.LoggerImpl=net.spy.memcached.compat.log.Log4JLogger" "-Dlog4j.configuration=file://$PWD/log4j.properties" -cp $LIB_CP:bin AsyncBopPipedInsertBulkTest $@
